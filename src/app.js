629ba7bca6354e14a57555bd57ec56dc3d2979e0
Nicolas Germone <90794504+nsgermone@users.noreply.github.com>
2022-09-28 13:45:23 -0300

    improve code & add env vars

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import employeeRoutes from './routes/employee.routes.js';
+import indexRoutes from './routes/index.routes.js';
+
+const app = express();
+
+app.use(express.json());
+
+app.use(indexRoutes);
+app.use('/api', employeeRoutes);
+
+/**
+ * TODO: Modify this routing
+ */
+app.use((req, res, next) => {
+  res.status(404).json({
+    message: 'endpoint not found',
+  });
+});
+
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,5 @@
-import express from 'express';
-import employeeRoutes from './routes/employee.routes.js';
-import indexRoutes from './routes/index.routes.js';
+import { PORT } from './config.js';
+import app from './app.js';
 
-const app = express();
-
-app.use(express.json());
-
-app.use(indexRoutes);
-app.use('/api', employeeRoutes);
-
-/**
- * TODO: Modify this routing
- */
-app.use((req, res, next) => {
-  res.status(404).json({
-    message: 'endpoint not found',
-  });
-});
-
-app.listen(3000);
+app.listen(PORT);
+console.log('Server running on port', PORT);
