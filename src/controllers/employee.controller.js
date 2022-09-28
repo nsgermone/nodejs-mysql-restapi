791407f6dbfc41450cb27924fac52a4d71f77fd7
Nicolas Germone <90794504+nsgermone@users.noreply.github.com>
2022-09-28 12:27:17 -0300

    Patch method insted of put employee

--- src/controllers/employee.controller.js
+++ src/controllers/employee.controller.js
@@ -1,34 +1,62 @@
-import { pool } from "./../db.js"
+import { pool } from './../db.js';
 
 export const getEmployees = async (req, res) => {
-    const [rows] = await pool.query('SELECT * FROM employee')
-    res.json(rows);
-}
+  const [rows] = await pool.query('SELECT * FROM employee');
+  res.json(rows);
+};
 
 export const getEmployee = async (req, res) => {
-    const [rows] = await pool.query('SELECT * FROM employee WHERE id = ?', [req.params.id])
-    if (rows.length <= 0) return res.status(404).json({
-        message: 'Employee not found'
-    })
-    res.send('employee found')
-}
+  const [rows] = await pool.query('SELECT * FROM employee WHERE id = ?', [
+    req.params.id,
+  ]);
+  if (rows.length <= 0)
+    return res.status(404).json({
+      message: 'Employee not found',
+    });
+  res.send('employee found');
+};
 
 export const createEmployee = async (req, res) => {
-    const { name, salary } = req.body
-    const [rows] = await pool.query('INSERT INTO employee (name, salary) VALUES (?, ?)', [name, salary])
-    res.send({ 
-        id: rows.insertId,
-        name,
-        salary,
-     })
-}
+  const { name, salary } = req.body;
+  const [rows] = await pool.query(
+    'INSERT INTO employee (name, salary) VALUES (?, ?)',
+    [name, salary]
+  );
+  res.send({
+    id: rows.insertId,
+    name,
+    salary,
+  });
+};
 
-export const updateEmployee = (req, res) => res.send('updating a member')
+export const updateEmployee = async (req, res) => {
+  const { id } = req.params;
+  const { name, salary } = req.body;
+
+  const [result] = await pool.query(
+    'UPDATE employee SET name = IFNULL(?, name), salary = IFNULL(?, salary) WHERE id = ?',
+    [name, salary, id]
+  );
+
+  console.log(result);
+
+  if (result.affectedRows === 0)
+    return res.status(404).json({
+      message: 'Employee not found',
+    });
+
+  const [rows] = await pool.query('SELECT * FROM employee WHERE id = ?', [id]);
+
+  res.json(rows[0]);
+};
 
 export const removeEmployee = async (req, res) => {
-    const [result] = await pool.query('DELETE FROM employee WHERE id = ?', [req.params.id])
-    if (result.affectedRows <= 0) return res.status(404).json({
-        message: 'Employee not found'
-    })
-    res.sendStatus(204)
-}
+  const [result] = await pool.query('DELETE FROM employee WHERE id = ?', [
+    req.params.id,
+  ]);
+  if (result.affectedRows <= 0)
+    return res.status(404).json({
+      message: 'Employee not found',
+    });
+  res.sendStatus(204);
+};
--- src/routes/employee.routes.js
+++ src/routes/employee.routes.js
@@ -9,7 +9,7 @@ router.get('/employees/:id', getEmployee)
 
 router.post('/employees', createEmployee)
 
-router.put('/employees', updateEmployee)
+router.patch('/employees/:id', updateEmployee)
 
 router.delete('/employees/:id', removeEmployee)
 
