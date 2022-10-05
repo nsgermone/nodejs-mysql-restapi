cc34fc72eb26b32771f90e91607638c5fcb6bc33
Nicolas Germone <90794504+nsgermone@users.noreply.github.com>
2022-10-05 11:03:36 -0300

    change port

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ import { config } from 'dotenv';
 
 config();
 
-export const PORT = process.env.PORT || 3000;
+export const PORT = process.env.PORT || 5000;
 
 export const DB_USER = process.env.DB_USER || 'root';
 
